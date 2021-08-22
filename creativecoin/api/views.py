e4c9fc1366b01ebc768286157f35b3451851ff23
Jepp Bautista <slapadasbas@gmail.com>
2021-08-22 13:47:15 +0800

    Converted api/wallet/qr to POST request

--- creativecoin/api/views.py
+++ creativecoin/api/views.py
@@ -3,24 +3,33 @@ from flask import Blueprint, redirect, request, url_for, render_template, jsonif
 from creativecoin.helper import utils
 from creativecoin import app, db, models
 
+import traceback
+
 import os
 
 api = Blueprint("api", __name__)
 
-@api.route("/api/wallet/qr", methods=["GET"], strict_slashes=False)
+@api.route("/api/wallet/qr", methods=["GET", "POST"], strict_slashes=False)
 def wallet_qr():
-    data = {}
+    try:
+        data = {"wallet": ""}
+        app.logger.error("TEST")
+
     
-    data["wallet"] = request.args.get("wallet", "")
-    if data["wallet"] == "":
-        return jsonify(message="Incomplete params"), 400
+        data = request.get_json()
+        if data["wallet"] == "" or data["wallet"] is None:
+            return jsonify(message="Incomplete params"), 400
 
-    wallet_address = utils.generate_wallet_id(data["wallet"])
-    png_path = os.path.join(os.getcwd(), f"creativecoin\\static\\image\\qr\\{wallet_address}.png")
-    if not os.path.isfile(png_path):
-        import pyqrcode
-        qr = pyqrcode.create(wallet_address)
-        qr.png(png_path, scale=8)
+        wallet_address = utils.generate_wallet_id(data["wallet"])
+        png_path = os.path.join(os.getcwd(), "creativecoin\\static\\image\\qr\\{}.png".format(wallet_address))
+        if not os.path.isfile(png_path):
+            import pyqrcode
+            qr = pyqrcode.create(wallet_address)
+            qr.png(png_path, scale=8)
 
-    return jsonify(wallet=f"static/image/qr/{wallet_address}.png")
+        return jsonify(wallet="static/image/qr/{}.png".format(wallet_address))
+    except Exception as e:
+        app.logger.error(traceback.format_exc())
+        return ""
+   
 
